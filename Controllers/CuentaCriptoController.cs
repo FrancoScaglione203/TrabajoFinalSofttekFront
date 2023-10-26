c4d5e42b27c74030da46cfa76b0eaa177ad9a7b0
FrancoScaglione203 <franco.scaglione2@gmail.com>
2023-10-25 23:04:33 -0300

    Se creo el Login Controller y se stablecio la conexion con la API del back, se empezo a crear Views y Controllers de CuentaFiduciaria y CuentaCripto

---
+++ Controllers/CuentaCriptoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrabajoFinalSofttekFront.Controllers
+{
+    public class CuentaCriptoController : Controller
+    {
+        public IActionResult CuentaCripto()
+        {
+            return View();
+        }
+    }
+}
